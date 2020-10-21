bea9da9ca883ee35946141016e6b4a0ff9fea3a0
Daniil <dshwets@gmail.com>
2020-10-21 16:53:41 +0600

    Добавил функцию преобразования url на репозиторий в путь к комитам для репозитория

--- source/history_service/views.py
+++ source/history_service/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def make_api_path(url):
+    url_parts = url.split('/')
+    if url_parts[2] == 'github.com':
+        api_url = 'https://api.github.com/repos/'+ url_parts[3]+'/'+ url_parts[4] + '/commits'
+        return api_url
+    elif url_parts[2] == 'gitlab.com':
+        api_url = 'https://gitlab.com/api/v4/projects/'+ url_parts[3]+'%2F'+ url_parts[4] \
+                  + '/repository/commits/'
+        return api_url
+
