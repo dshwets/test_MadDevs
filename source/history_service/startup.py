cf325d5bbce5b1ca46e942da0e86036c1d47c380
Daniil <dshwets@gmail.com>
2020-10-28 11:24:04 +0600

    Выделил цикл в отедельный поток, теперь не надо комментировать  middleware.

--- source/history_service/startup.py
+++ source/history_service/startup.py
@@ -1,17 +1,27 @@
 import schedule
 import time
+import threading
 from history_service.views import BulkCommitSaver
 
 
 class StartupMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        def job():
-            BulkCommitSaver().save_or_create_commits()
 
-        # schedule.every(2).minutes.do(job)  # для тестов
-        schedule.every().day.at("10:30").do(job)
+        def starter():
+            def run_timer():
+                def job():
+                    BulkCommitSaver().save_or_create_commits()
 
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+                # schedule.every(30).seconds.do(job)  # для тестов
+                schedule.every().day.at("10:30").do(job)
+
+                while True:
+                    schedule.run_pending()
+                    # print('111111')
+                    time.sleep(1)
+
+            x = threading.Thread(target=run_timer, daemon=True)
+            x.start()
+        starter()
+
